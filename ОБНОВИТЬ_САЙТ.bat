21675bfd206476a5026f56a6155da9a1375e397e
Natalia <rutasha2809@gmail.com>
2026-05-16 16:33:49 +0300

    Pion: fix description, add rozovyy-pion aroma, single aroma option

--- ОБНОВИТЬ_САЙТ.bat
+++ ОБНОВИТЬ_САЙТ.bat
@@ -17,7 +17,7 @@ git status --short
 
 echo.
 echo === Step 3: Commit ===
-git commit -m "Pion conteiner: add description, dimensions, burnTime"
+git commit -m "Pion: fix description, add rozovyy-pion aroma, single aroma option"
 if %errorlevel% neq 0 ( echo ERROR at commit && pause && exit /b 1 )
 
 echo.
