d5f036600fda48988cdb2b31f7ea5abe32b263dd
Natalia <rutasha2809@gmail.com>
2026-05-17 18:48:18 +0300

    Tykva: add colors white, beige

--- ОБНОВИТЬ_САЙТ.bat
+++ ОБНОВИТЬ_САЙТ.bat
@@ -17,7 +17,7 @@ git status --short
 
 echo.
 echo === Step 3: Commit ===
-git commit -m "Roza Intalia: add colors white, pink, beige"
+git commit -m "Tykva: add colors white, beige"
 if %errorlevel% neq 0 ( echo ERROR at commit && pause && exit /b 1 )
 
 echo.
