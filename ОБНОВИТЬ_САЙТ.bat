daf1b190b7358d3981b6fa4ee7d1eaf7cdbd4579
Natalia <rutasha2809@gmail.com>
2026-05-17 17:40:53 +0300

    Romashka: description, dimensions, burnTime, price, materials, aromas

--- ОБНОВИТЬ_САЙТ.bat
+++ ОБНОВИТЬ_САЙТ.bat
@@ -17,7 +17,7 @@ git status --short
 
 echo.
 echo === Step 3: Commit ===
-git commit -m "Kuvshinka: fix burnTime to more than 16 hours"
+git commit -m "Romashka: description, dimensions, burnTime, price, materials, aromas"
 if %errorlevel% neq 0 ( echo ERROR at commit && pause && exit /b 1 )
 
 echo.
