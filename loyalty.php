61f3d26ef524ad0e871f9f51bc4d45fd1f6ee155
Natalia <rutasha2809@gmail.com>
2026-06-08 20:50:50 +0300

    Update site

--- loyalty.php
+++ loyalty.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/config.php';
 
-header('Access-Control-Allow-Origin: ' . ALLOWED_ORIGIN);
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json; charset=utf-8');
